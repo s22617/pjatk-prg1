48ebd6676ae094508900108a55121bce1bdc2b9f
Daniel <s22617@pjwstk.edu.pl>
2021-02-08 15:40:13 +0100

    Update #2

--- 5_io/s06-file-server.cpp
+++ 5_io/s06-file-server.cpp
@@ -1,83 +1,72 @@
-#include <iostream>
-#include <string>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
 #include <arpa/inet.h>
+#include <fcntl.h>
+#include <netinet/in.h>
 #include <string.h>
-
+#include <sys/socket.h>
+#include <sys/types.h>
 #include <unistd.h>
 
-#include <fcntl.h>
+#include <array>
+#include <iostream>
+#include <string>
 auto main() -> int
 {
     auto sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
-    {
+    if (sock == -1) {
         perror("Can't create a socket");
         return 1;
     }
-    auto const ip = std::string{"127.0.0.1"};
+    auto const ip   = std::string{"127.0.0.1"};
     auto const port = uint16_t{8080};
 
     sockaddr_in addr;
-    //prepare the address
-    
+    // prepare the address
+
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htobe16(port);
+    addr.sin_port   = htobe16(port);
     inet_pton(addr.sin_family, ip.c_str(), &addr.sin_addr);
-    //server
+    // server
     bind(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
     listen(sock, SOMAXCONN);
-    
 
-    
+
     auto client_sock = accept(sock, nullptr, nullptr);
-    
+
     close(sock);
 
-    char buf[4096];
-    while (true){
+    std::array<char, 4096> buf{0};
+    auto message = std::string{};
+    auto file_output = std::string{};
+    while (true) {
+        memset(buf.data(), 0, sizeof(buf));
+        auto bytesReceived = read(client_sock, buf.data(), sizeof(buf));
 
-        memset(buf, 0, 4096);
-        int bytesReceived = read(client_sock, buf, 4096);
-        
-        if (bytesReceived == -1)
-        {
+        if (bytesReceived == -1) {
             perror("Error. Quitting\n");
             break;
         }
-        if (bytesReceived == 0)
-        {
+        if (bytesReceived == 0) {
             std::cout << "Client disconnected\n";
             break;
         }
-        auto message = std::string(buf, 0, bytesReceived);
-        std::cout << message 
-                    << std::endl;
-        if (access(message.c_str(), F_OK) != 0){
+        message = std::string(buf.data(), 0, bytesReceived);
+        std::cout << message << std::endl;
+        if (access(message.c_str(), F_OK) != 0) {
             auto alert = std::string{":("};
-            strcpy(buf, alert.c_str());
-            write(client_sock, buf, alert.length());
-        }
-        else {
-            auto fd = open(message.c_str(), O_RDWR, S_IRUSR | S_IWUSR);
-            auto const n = read(fd, buf, 4096);
+            write(client_sock, alert.data(), alert.length());
+        } else {
+            auto fd      = open(message.c_str(), O_RDWR, S_IRUSR | S_IWUSR);
+            auto const n = read(fd, buf.data(), sizeof(buf));
             if (n == -1) {
                 perror("read(2)");
-            }
-            else{
-                auto file_output = std::string{};
-                file_output = message + "\n" + std::string{buf, static_cast<size_t>(n)};
-                strcpy(buf, file_output.c_str());
-                write(client_sock, buf, file_output.length() - 1);
-                
-                //std::cout << file_output << "\n";
+            } else {
+                file_output      = message + "\n"
+                              + std::string{buf.data(), static_cast<size_t>(n)};
+                write(
+                    client_sock, file_output.data(), file_output.length() - 1);
             }
         }
-        
     }
 
     shutdown(sock, SHUT_RDWR);
--- 5_io/s06-random-note-server.cpp
+++ 5_io/s06-random-note-server.cpp
@@ -1,71 +1,67 @@
+#include <arpa/inet.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+#include <array>
 #include <iostream>
 #include <string>
-
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <string.h>
-
-#include <unistd.h>
 #include <vector>
 
 auto main() -> int
 {
     auto sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1)
-    {
+    if (sock == -1) {
         perror("Can't create a socket");
         return 1;
     }
-    auto const ip = std::string{"127.0.0.1"};
+    auto const ip   = std::string{"127.0.0.1"};
     auto const port = uint16_t{8080};
 
     sockaddr_in addr;
-    //prepare the address
-    
+    // prepare the address
+
     memset(&addr, 0, sizeof(addr));
     addr.sin_family = AF_INET;
-    addr.sin_port = htobe16(port);
+    addr.sin_port   = htobe16(port);
     inet_pton(addr.sin_family, ip.c_str(), &addr.sin_addr);
-    //server
+    // server
     bind(sock, reinterpret_cast<sockaddr*>(&addr), sizeof(addr));
     listen(sock, SOMAXCONN);
-     
-    auto client_sock = accept(sock, nullptr, nullptr);
     
-    close(sock);
+    std::vector<std::string> messages{};
+        
+    while (true) {
 
-    std::vector<std::string> messages {};
-    char buf[4096];
-    while (true){
+        auto client_sock = accept(sock, nullptr, nullptr);
+        std::array<char, 4096> buf{0};
 
-        memset(buf, 0, 4096);
-        int bytesReceived = read(client_sock, buf, 4096);
-        
-        if (bytesReceived == -1)
-        {
-            perror("Error. Quitting\n");
-            break;
+        while (true) {
+            memset(buf.data(), 0, sizeof(buf));
+            auto bytesReceived = read(client_sock, buf.data(), sizeof(buf));
+
+            if (bytesReceived == -1) {
+                perror("Error. Quitting\n");
+                break;
+            }
+            if (bytesReceived == 0) {
+                std::cout << "Client disconnected\n";
+                break;
+            }
+            auto message = std::string(buf.data(), 0, bytesReceived);
+            std::cout << message << std::endl;
+            messages.push_back(message);
+            auto random = messages[rand() % messages.size()];
+
+            write(client_sock, random.data(), random.length());
         }
-        if (bytesReceived == 0)
-        {
-            std::cout << "Client disconnected\n";
-            break;
-        }
-        auto message = std::string(buf, 0, bytesReceived);
-        std::cout << message
-                    << std::endl;
-        messages.push_back(message);
-        auto random = messages[rand() % messages.size()];
-        
-        std::cout << random << "\n";
-        
-        write(client_sock, buf, bytesReceived + 1);
+        close(client_sock);
     }
-   
+
+    close(sock);
     shutdown(sock, SHUT_RDWR);
-    close(client_sock);
 
     return 0;
 }
